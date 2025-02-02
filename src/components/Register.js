74b72e43732ef1772be4fab5ab51f6f7ac6969f4
vasilecarp <vasilecarpconsulting@gmail.com>
2025-02-02 12:31:46 +0200

    add styles for CandidateProfile, Navbar, and Register components

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import "./Register.css";
 
 const Register = () => {
   const [email, setEmail] = useState("");
